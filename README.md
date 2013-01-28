4918ee7576eb91ac79f78bd57c9605110c8d9b5f
Micky Hulse <micky@registerguard.com>
2013-01-27 20:17:03 -0800

    Hello world!

---
+++ README.md
@@ -0,0 +1,25 @@
+# jQuery Kerplop!
+
+### Move stuff from one place to another.
+
+More information coming soon.
+
+---
+
+#### DEMO
+
+[![qr code](http://chart.apis.google.com/chart?cht=qr&chl=https://github.com/registerguard/jquery-kerplop/&chs=240x240)](http://registerguard.github.com/jquery-kerplop/demo/)
+
+(Scan QR code with phone and/or click to [view the latest demo](http://registerguard.github.com/jquery-kerplop/demo/).)
+
+---
+
+#### LEGAL
+
+Copyright © 2013 [Micky Hulse](http://hulse.me)/[The Register-Guard](http://registerguard.com)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ kerplop/jquery.kerplop.js
@@ -0,0 +1,166 @@
+/**
+ * jQuery Kerplop!
+ * Move stuff from one place to another.
+ *
+ * @author    Micky Hulse
+ * @link      http://hulse.me
+ * @docs      http://github.com/registerguard/jquery-kerplop
+ * @copyright Copyright (c) 2013 Micky Hulse.
+ * @license   Released under the Apache License, Version 2.0.
+ * @version   1.0.0
+ * @date      2013/01/27
+ */
+
+//----------------------------------
+
+// Notes to self:
+//console.profile('profile foo');
+// ... code here ...
+//console.profileEnd('profile foo');
+// ... or:
+// console.time('timing foo');
+// ... code here ...
+// console.timeEnd('timing foo');
+
+//----------------------------------
+
+;(function($, window, document, undefined) {
+	
+	'use strict';
+	
+	var console = window.console || { log : function() {}, warn : function() {} },
+	
+	defaults = {
+		
+		use  : 'html',
+		flag : 'kerploped',
+		
+		onInit      : $.noop,
+		onAfterInit : $.noop
+		
+	},
+	
+	methods = {
+		
+		init: function(options) {
+			
+			return this.each(function() {
+				
+				var settings = $.extend({}, defaults, options),
+				$this        = $(this),
+				data         = $this.data('kerplop'),
+				data_from    = $this.data('kerplop-from'),
+				data_to      = $this.data('kerplop-to'),
+				data_use     = $this.data('kerplop-use'),
+				data_flag    = $this.data('kerplop-flag'),
+				$from        = ((data_from) ? $('#' + data_from) : ""),
+				$to          = ((data_to) ? $('#' + data_to) : ""),
+				use          = ((data_use && (/^(?:after|append|before|html|prepend|text)$/).test(data_use)) ? data_use : settings.use),
+				flag         = (data_flag || settings.flag);
+				
+				if ( ! data) {
+					
+					$this.data('kerplop', {
+						
+						this     : $this,
+						from     : $from,
+						to       : $to,
+						use      : use,
+						flag     : flag,
+						settings : settings,
+						init     : false
+						
+					});
+					data = $this.data('kerplop');
+					
+				}
+				
+				if ( ! data.init) {
+					
+					data.init = true;
+					
+					settings.onInit.call($this, $from, $to);
+					
+					/**
+					 * Reference:
+					 *
+					 * $('.inner').after('<p>Test</p>');
+					 * $('.inner').append('<p>Test</p>');
+					 * $('.inner').before('<p>Test</p>');
+					 * $('.inner').html('<p>Test</p>');
+					 * $('.inner').prepend('<p>Test</p>');
+					 * $('.inner').text('<p>Test</p>');
+					 *
+					 * @see http://api.jquery.com/category/manipulation/
+					 */
+					
+					if ($from) {
+						
+						$this[use]($from.addClass(flag).html());
+						
+					}
+					
+					if ($to) {
+						
+						$to[data.use]($this.addClass(flag).html());
+						
+					}
+					
+					settings.onAfterInit.call($this, $from, $to);
+					
+				} else {
+					
+					console.warn('kerplop already initialized on', this);
+					
+					return this;
+					
+				}
+				
+			});
+			
+		},
+		
+		destroy: function() {
+			
+			return this.each(function() {
+				
+				var $this = $(this),
+				data      = $this.data('kerplop');
+				
+				if (data) {
+					
+					(data.from || data.to).removeClass(data.flag);
+					
+					$this.removeData('kerplop'); // Move along. Nothing to see here.
+					
+					// What's the best way to restore things back to their original states?
+					
+				}
+			
+			});
+			
+		}
+		
+	};
+	
+	//--------------------------------------------------------------------
+	
+	$.fn.kerplop = function(method) {
+		
+		if (methods[method]) {
+			
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+			
+		} else if ((typeof method === 'object') || ( ! method)) {
+			
+			return methods.init.apply(this, arguments);
+			
+		} else {
+			
+			$.error('Method ' + method + ' does not exist on jQuery.kerplop.');
+			
+		}
+		
+	};
+	
+}(jQuery, window, document));

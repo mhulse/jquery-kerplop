0c07345488317a76dcc1f9331cd542b991363d03
Micky Hulse <micky@registerguard.com>
2013-02-11 19:56:23 -0800

    House cleaning.

--- kerplop/jquery.kerplop.js
+++ kerplop/jquery.kerplop.js
@@ -24,145 +24,336 @@
 
 ;(function($, window, document, undefined) {
 	
-	'use strict';
+	'use strict'; // Lint setting.
+	
+	//--------------------------------------------------------------------------
+	//
+	// Local "globals":
+	//
+	//--------------------------------------------------------------------------
+	
+	/**
+	 * Javascript console.
+	 *
+	 * @see rgne.ws/12p2bvl
+	 */
 	
 	var console = window.console || { log : function() {}, warn : function() {} },
 	
+	/**
+	 * The plugin namespace.
+	 */
+	
+	NS = 'kerplop',
+	
+	//--------------------------------------------------------------------------
+	//
+	// Defaults and settings:
+	//
+	//--------------------------------------------------------------------------
+	
 	defaults = {
 		
-		from : '',
-		to   : '',
-		use  : 'html',
-		flag : 'kerplopped',
+		from : '',           // Element, ID name, to copy from. Used if `data-from` local is not defined.
+		to   : '',           // Element, ID name, to copy to. Used if `data-to` local is not defined.
+		use  : 'html',       // Replacement function to use when copying content from, or to, other elements. Allowed values are `after`, `append`, `before`, `html` (default), `prepend` and `text`.
+		flag : 'kerplopped', // CSS class name to apply to element that's copied from, or the the element that's copying its contents to another element; useful for when you want to hide the `kerplopped` element using CSS techniques.
 		
-		onInit      : $.noop,
-		onAfterInit : $.noop
+		onInit      : $.noop, // Callback on plugin initialization; this function gets passed two arguments 1) the "from" element 2) the "to" element and "this" is the context of the current element.
+		onAfterInit : $.noop  // Callback after plugin initialization; this function gets passed two arguments 1) the "from" element 2) the "to" element and "this" is the context of the current element.
 		
-	},
+	}, // defaults
+	
+	//--------------------------------------------------------------------------
+	//
+	// Public methods:
+	//
+	//--------------------------------------------------------------------------
+	
+	/**
+	 * Methods object.
+	 *
+	 * @type { object }
+	 */
 	
 	methods = {
 		
-		init: function(options) {
+		/**
+		 * Init constructor.
+		 *
+		 * @type { function }
+		 * @param { object } opts Options object literal.
+		 * @this { object.jquery }
+		 * @return { object.jquery } Returns target object(s) for chaining purposes.
+		 */
+		
+		init : function(options) {
+			
+			//----------------------------------
+			// Loop & return each this:
+			//----------------------------------
 			
 			return this.each(function() {
 				
-				var settings = $.extend({}, defaults, options),
-				$this        = $(this),
-				data         = $this.data('kerplop');
+				//----------------------------------
+				// Local variable(s):
+				//----------------------------------
+				
+				var $this    = $(this),                         // Target object.
+				    data     = $this.data(NS),                  // Namespace instance data.
+				    settings = $.extend({}, defaults, options); // Merge defaults and options.
+				
+				//----------------------------------
+				// Initialize data:
+				//----------------------------------
 				
 				if ( ! data) {
 					
-					var data_from = $this.data('kerplop-from'),
-					data_to       = $this.data('kerplop-to'),
-					data_use      = $this.data('kerplop-use'),
-					data_flag     = $this.data('kerplop-flag'),
-					$from         = ((data_from) ? $('#' + data_from) : ((settings.from.length) ? $('#' + settings.from) : '')),
-					$to           = ((data_to) ? $('#' + data_to) : ((settings.to.length) ? $('#' + settings.to) : '')),
-					use           = ((data_use && (/^(?:after|append|before|html|prepend|text)$/).test(data_use)) ? data_use : settings.use),
-					flag          = (data_flag || settings.flag);
+					//----------------------------------
+					// Setup variables:
+					//----------------------------------
 					
-					$this.data('kerplop', {
+					var data_from = $this.data('kerplop-from'), // See `defaults.from` docs.
+					    data_to   = $this.data('kerplop-to'),   // See `defaults.to` docs.
+					    data_use  = $this.data('kerplop-use'),  // See `defaults.use` docs.
+					    data_flag = $this.data('kerplop-flag'); // See `defaults.flag` docs.
+					
+					//----------------------------------
+					// Namespaced instance data:
+					//----------------------------------
+					
+					$this.data(NS, {
 						
-						target   : $this,
-						from     : $from,
-						to       : $to,
-						use      : use,
-						flag     : flag,
+						flag     : (data_flag || settings.flag),
+						from     : ((data_from) ? $('#' + data_from) : ((settings.from.length) ? $('#' + settings.from) : '')),
+						init     : false,
 						settings : settings,
-						init     : false
+						target   : $this,
+						to       : ((data_to) ? $('#' + data_to) : ((settings.to.length) ? $('#' + settings.to) : '')),
+						use      : ((data_use && (/^(?:after|append|before|html|prepend|text)$/).test(data_use)) ? data_use : settings.use)
 						
 					});
 					
-					data = $this.data('kerplop');
+					//----------------------------------
+					// Easy access:
+					//----------------------------------
+					
+					data = $this.data(NS);
 					
 				}
 				
+				//----------------------------------
+				// Data initialization check:
+				//----------------------------------
+				
 				if ( ! data.init) {
 					
-					data.init = true;
+					//----------------------------------
+					// Call main:
+					//----------------------------------
 					
-					data.settings.onInit.call(data.target, data.from, data.to);
-					
-					/**
-					 * Reference:
-					 *
-					 * $('.inner').after('<p>Test</p>');
-					 * $('.inner').append('<p>Test</p>');
-					 * $('.inner').before('<p>Test</p>');
-					 * $('.inner').html('<p>Test</p>');
-					 * $('.inner').prepend('<p>Test</p>');
-					 * $('.inner').text('<p>Test</p>');
-					 *
-					 * @see http://api.jquery.com/category/manipulation/
-					 */
-					
-					if (data.from.length) {
-						
-						data.target[data.use](data.from.addClass(data.flag).html());
-						
-					}
-					
-					if (data.to.length) {
-						
-						data.to[data.use](data.target.addClass(data.flag).html());
-						
-					}
-					
-					data.settings.onAfterInit.call(data.target, data.from, data.to);
+					_main.call($this, data);
 					
 				} else {
 					
-					console.warn('Kerplop already initialized on', this);
+					//----------------------------------
+					// Ouch!
+					//----------------------------------
 					
-					return this;
+					console.warn('jQuery.' + NS, 'thinks it\'s already initialized on', this);
+					
+					//return this; // Needed?
 					
 				}
 				
 			});
 			
-		},
+		}, // init
 		
-		destroy: function() {
+		//----------------------------------
+		
+		/**
+		 * Removes plugin from element.
+		 *
+		 * @type { function }
+		 * @this { object.jquery }
+		 * @return { object.jquery } Returns target object(s) for chaining purposes.
+		 */
+		
+		destroy : function() {
+			
+			//----------------------------------
+			// Loop & return each this:
+			//----------------------------------
 			
 			return this.each(function() {
 				
+				//----------------------------------
+				// Local variable(s):
+				//----------------------------------
+				
 				var $this = $(this),
-				data      = $this.data('kerplop');
+				    data  = $this.data(NS);
+				
+				//----------------------------------
+				// Data?
+				//----------------------------------
 				
 				if (data) {
 					
+					//----------------------------------
+					// Remove CSS class:
+					//----------------------------------
+					
 					(data.from || data.to).removeClass(data.flag);
 					
-					$this.removeData('kerplop'); // Move along. Nothing to see here.
+					//----------------------------------
+					// Namespaced instance data:
+					//----------------------------------
+					
+					$this.removeData(NS); // Move along. Nothing to see here.
 					
 					// What's the best way to restore things back to their original states?
 					
 				} // ... else warn that this hasn't been initialzed with kerplop?
-			
+				
 			});
+			
+		} // destroy
+		
+	}, // methods
+	
+	//--------------------------------------------------------------------------
+	//
+	// Private methods:
+	//
+	//--------------------------------------------------------------------------
+	
+	/**
+	 * Called after plugin initialization.
+	 *
+	 * @private
+	 * @type { function }
+	 * @this { object.jquery }
+	 */
+	
+	_main = function(data) {
+		
+		//----------------------------------
+		// Data?
+		//----------------------------------
+		
+		if (typeof data == 'undefined') {
+			
+			//----------------------------------
+			// Attempt to determine data:
+			//----------------------------------
+			
+			data = this.data(NS);
 			
 		}
 		
-	};
+		//----------------------------------
+		// Data?
+		//----------------------------------
+		
+		if (data) {
+			
+			//----------------------------------
+			// Yup!
+			//----------------------------------
+			
+			data.init = true; // Data initialization flag.
+			
+			//----------------------------------
+			// Callback:
+			//----------------------------------
+			
+			data.settings.onInit.call(data.target, data.from, data.to);
+			
+			/**
+			 * Reference:
+			 *
+			 * $('.inner').after('<p>Test</p>');
+			 * $('.inner').append('<p>Test</p>');
+			 * $('.inner').before('<p>Test</p>');
+			 * $('.inner').html('<p>Test</p>');
+			 * $('.inner').prepend('<p>Test</p>');
+			 * $('.inner').text('<p>Test</p>');
+			 *
+			 * @see http://api.jquery.com/category/manipulation/
+			 */
+			
+			//----------------------------------
+			// From?
+			//----------------------------------
+			
+			if (data.from.length) {
+				
+				//----------------------------------
+				// Yup! Copy "from" & apply class:
+				//----------------------------------
+				
+				data.target[data.use](data.from.addClass(data.flag).html());
+				
+			}
+			
+			//----------------------------------
+			// To?
+			//----------------------------------
+			
+			if (data.to.length) {
+				
+				//----------------------------------
+				// Yup! Copy "to" & apply class:
+				//----------------------------------
+				
+				data.to[data.use](data.target.addClass(data.flag).html());
+				
+			}
+			
+			//----------------------------------
+			// Callback:
+			//----------------------------------
+			
+			data.settings.onAfterInit.call(data.target, data.from, data.to);
+			
+		}
+		
+	}; // _main
 	
-	//--------------------------------------------------------------------
+	//--------------------------------------------------------------------------
+	//
+	// Method calling logic:
+	//
+	//--------------------------------------------------------------------------
 	
-	$.fn.kerplop = function(method) {
+	/**
+	 * Boilerplate plugin logic.
+	 *
+	 * @constructor
+	 * @see rgne.ws/OvKpPc
+	 * @type { function }
+	 * @param { string } method String method identifier.
+	 * @return { method } Calls plugin method with supplied params.
+	 */
+	
+	$.fn[NS] = function(method) {
 		
 		if (methods[method]) {
 			
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 			
-		} else if ((typeof method === 'object') || ( ! method)) {
+		} else if ((typeof method == 'object') || ( ! method)) {
 			
 			return methods.init.apply(this, arguments);
 			
 		} else {
 			
-			$.error('Method ' + method + ' does not exist on jQuery.kerplop.');
+			$.error('jQuery.' + NS + ' thinks that ' + method + ' doesn\'t exist'); // Should I override? rgne.ws/MwgkP8
 			
 		}
 		
-	};
+	}; // $.fn[NS]
 	
 }(jQuery, window, document));
